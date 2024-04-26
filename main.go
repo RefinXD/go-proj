72974e56700fb7410517ff63d1c3ee98a3b5a792
Anik Romyanon <anikromyanon@Aniks-MacBook-Air.local>
2024-04-26 15:10:42 +0700

    fixed router path

--- main.go
+++ main.go
@@ -4,27 +4,22 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/RefinXD/go-proj/controllers"
-	"github.com/RefinXD/go-proj/service"
+	"github.com/RefinXD/go-proj/router"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 
-    empService := service.Instantiate()
-    empHandler := controllers.NewEmployeeController(empService)
-    
     r := chi.NewRouter()
     r.Use(middleware.Logger)
     r.Use(middleware.Recoverer)
-
-
     r.Get("/", func(w http.ResponseWriter, r *http.Request) {
         w.Write([]byte("Hello World!"))
 
     })
-    empHandler.SetRouter(r)
+    api_router := router.InitRouter()
+    r.Mount("/api",api_router)
     fmt.Println("starting")
     http.ListenAndServe(":3000", r)
 }
--- router/router.go
+++ router/router.go
@@ -1,13 +1,17 @@
 package router
 
 import (
-	// "github.com/RefinXD/go-proj/controllers"
+	"github.com/RefinXD/go-proj/controllers"
+	"github.com/RefinXD/go-proj/service"
 	"github.com/go-chi/chi/v5"
 )
 
 func InitRouter() *chi.Mux {
 	api_router := chi.NewRouter()
 	//api_router.Get("/employees",controllers.EmployeeController.GetAllHandler)
+	empService := service.Instantiate()
+    empHandler := controllers.NewEmployeeController(empService)
+	empHandler.SetRouter(api_router)
 
 	return api_router
 }
